51843739d78fa3c0e17e05d1a3e81f2d43e8257e
Sushant Gandhi <sushant@simplified.co>
2025-02-22 22:11:55 +0530

    fixed landing page bg

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react";
 
 export default function PublicLayout({ children }: { children: ReactNode }) {
-  return <div className="h-full dark:bg-[#1f1f1f]">{children}</div>;
+  return (
+    <div className="h-full dark:bg-[#1f1f1f] overflow-y-auto">{children}</div>
+  );
 }
