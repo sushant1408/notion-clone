a9206dd18fee6b8e01cc2a91c18fe48b239af707
Sushant Gandhi <sushant@simplified.co>
2025-02-22 22:14:52 +0530

    fixed landing page logo

--- src/app/(marketing)/_components/navbar.tsx
+++ src/app/(marketing)/_components/navbar.tsx
@@ -22,7 +22,9 @@ const Navbar = () => {
         scrolled && "border-b shadow-sm"
       )}
     >
-      <Logo />
+      <div className="shrink-0">
+        <Logo />
+      </div>
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
         {isLoading && <Spinner />}
         {!isAuthenticated && !isLoading && (
